1a098663355b0940cab6b1b12d2715e12b4356d3
David (Oluwasegun) Rufai <ysenruf@gmail.com>
2024-11-11 12:54:14 +0100

    complete UI

--- app/src/main/java/com/xheghun/xchange/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/xheghun/xchange/ui/screens/HomeScreen.kt
@@ -16,13 +16,20 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.KeyboardArrowDown
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -31,7 +38,9 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
@@ -41,16 +50,18 @@ import com.xheghun.xchange.ui.theme.OpenSans
 import com.xheghun.xchange.ui.theme.colorBackground
 import com.xheghun.xchange.ui.theme.colorBlue
 import com.xheghun.xchange.ui.theme.colorGray
+import com.xheghun.xchange.ui.theme.colorGrey
 
 @Composable
 fun HomeScreen(navController: NavController) {
-
+    var value by remember { mutableStateOf("") }
     Column(
         Modifier
             .background(colorBackground)
             .padding(horizontal = 12.dp)
 
     ) {
+        //HEADER
         Column(
             horizontalAlignment = Alignment.CenterHorizontally,
             verticalArrangement = Arrangement.Center,
@@ -68,13 +79,13 @@ fun HomeScreen(navController: NavController) {
             )
         }
 
+        //BODY INPUT
         Column(
             Modifier
                 .padding(vertical = 20.dp)
                 .clip(RoundedCornerShape(12.dp))
                 .shadow(2.dp, RoundedCornerShape(12.dp))
                 .background(Color.White)
-                .weight(5f)
                 .fillMaxWidth()
                 .padding(15.dp)
         ) {
@@ -82,26 +93,8 @@ fun HomeScreen(navController: NavController) {
             val exchangeCurrency = Currency("GBP")
             Text("Amount", color = colorGray)
 
-            Row(
-                verticalAlignment = Alignment.CenterVertically,
-                modifier = Modifier.padding(vertical = 10.dp)
-            ) {
-                Image(
-                    painter = painterResource(id = R.drawable.ic_launcher_background),
-                    contentDescription = "base currency image",
-                    modifier = Modifier
-                        .clip(CircleShape)
-                        .size(40.dp)
-                )
-                Box(Modifier.width(6.dp))
-                Text(
-                    text = baseCurrency.code,
-                    color = colorBlue,
-                    fontSize = 18.sp,
-                    style = TextStyle(fontFamily = OpenSans, fontWeight = FontWeight.Medium)
-                )
-                Icon(Icons.Default.KeyboardArrowDown, contentDescription = "")
-                Box(Modifier.width(6.dp))
+            CurrencyLayout(currencyCode = baseCurrency.code, value = value) { newValue ->
+                value = newValue
             }
 
             Box(Modifier.fillMaxWidth()) {
@@ -129,8 +122,13 @@ fun HomeScreen(navController: NavController) {
 
             Text("Converted Amount", color = colorGray)
 
+            CurrencyLayout(currencyCode = baseCurrency.code, value = value) { newValue ->
+                value = newValue
+            }
+
         }
 
+        //RESULT
         Column(Modifier.weight(3.5f)) {
             Text(text = "Indicative Exchange Rate", color = colorGray)
             Text(
@@ -143,7 +141,66 @@ fun HomeScreen(navController: NavController) {
     }
 }
 
-@Composable
-fun XTextField() {
 
+@Composable
+fun CurrencyLayout(
+    currencyCode: String,
+    value: String,
+    onValueChanged: (String) -> Unit
+) {
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = Modifier.padding(vertical = 10.dp)
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.ic_launcher_background),
+            contentDescription = "base currency image",
+            modifier = Modifier
+                .clip(CircleShape)
+                .size(40.dp)
+        )
+        Box(Modifier.width(10.dp))
+        Text(
+            text = currencyCode,
+            color = colorBlue,
+            fontSize = 18.sp,
+            style = TextStyle(fontFamily = OpenSans, fontWeight = FontWeight.Medium)
+        )
+        Icon(Icons.Default.KeyboardArrowDown, contentDescription = "")
+        Box(Modifier.width(10.dp))
+
+        CurrencyInput(
+            value = value,
+            onValueChanged = onValueChanged::invoke,
+            modifier = Modifier.weight(1f)
+        )
+    }
+}
+
+@Composable
+fun CurrencyInput(value: String, onValueChanged: (String) -> Unit, modifier: Modifier) {
+    BasicTextField(
+        value = value,
+        singleLine = true,
+        keyboardOptions = KeyboardOptions.Default.copy(keyboardType = KeyboardType.Number),
+        onValueChange = onValueChanged::invoke,
+        decorationBox = { innerTextField ->
+            if (value.isEmpty()) {
+                Text(
+                    text = "",
+                    maxLines = 1,
+                    overflow = TextOverflow.Ellipsis,
+                    color = Color.Gray,
+                    style = MaterialTheme.typography.bodySmall,
+                )
+            } else {
+                innerTextField()
+            }
+
+        },
+        modifier = modifier
+            .clip(RoundedCornerShape(6.dp))
+            .background(colorGrey)
+            .padding(vertical = 10.dp)
+    )
 }
--- app/src/main/java/com/xheghun/xchange/ui/theme/Color.kt
+++ app/src/main/java/com/xheghun/xchange/ui/theme/Color.kt
@@ -4,4 +4,5 @@ import androidx.compose.ui.graphics.Color
 
 val colorBlue = Color(0XFF1F2261)
 val colorGray = Color(0XFF808080)
+val colorGrey = Color(0XFFEFEFEF)
 val colorBackground = Color(0XFFF6F6F6)
